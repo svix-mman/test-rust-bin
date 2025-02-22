75a35e94fcc899cf79b682a91fb1992a2403b35a
Mendy Man <mman@svix.com>
2025-02-22 18:45:50 -0500

    clean up

--- generate_release_files.sh
+++ generate_release_files.sh
@@ -13,16 +13,16 @@ if [ "$(uname)" == "Darwin" ]; then
 else
   TAR_BIN="tar"
 fi
+BIN_NAME="test-rust-bin"
+TAR_FILE="target/distrib/$BIN_NAME-$1.tar.xz"
 mkdir -p target/distrib
-
-TAR_FILE="target/distrib/test-rust-bin-$1.tar.xz"
 
 $TAR_BIN -czf \
     $TAR_FILE \
     README.md \
     LICENSE \
-    --transform="s|target/$1/release/test-rust-bin|test-rust-bin|" \
-    "target/$1/release/test-rust-bin"
+    --transform="s|target/$1/release/$BIN_NAME|$BIN_NAME|" \
+    "target/$1/release/$BIN_NAME"
 
 sha256sum $TAR_FILE > "$TAR_FILE.sha256"
 
