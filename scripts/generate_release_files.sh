9b924d4373d46d5392c04daf64d667865bd12cb6
Mendy Man <mman@svix.com>
2025-02-22 19:39:06 -0500

    add windows arm

--- scripts/generate_release_files.sh
+++ scripts/generate_release_files.sh
@@ -18,7 +18,9 @@ fi
 
 BIN_NAME="test-rust-bin"
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-  BIN_NAME="$BIN_NAME.exe"
+  BIN_FILE="$BIN_NAME.exe"
+  else
+  BIN_FILE="$BIN_NAME"
 fi
 
 TAR_FILE="target/distrib/$BIN_NAME-$1.tar.xz"
@@ -28,8 +30,8 @@ $TAR_BIN -czf \
     $TAR_FILE \
     README.md \
     LICENSE \
-    --transform="s|target/$1/release/$BIN_NAME|$BIN_NAME|" \
-    "target/$1/release/$BIN_NAME"
+    --transform="s|target/$1/release/$BIN_FILE|$BIN_FILE|" \
+    "target/$1/release/$BIN_FILE"
 
 sha256sum $TAR_FILE > "$TAR_FILE.sha256"
 
